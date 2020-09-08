c628049039aa2416bfe5c0255fc7a7204c8f65cb
Chinedu Ifediorah <cifediorah3@gmail.com>
2020-09-08 10:27:37 +0100

    feat: bineary search algoorithm

---
+++ binarySearch/index.js
@@ -0,0 +1,29 @@
+// // note : binary search only work for sorted array
+
+function binarySearch(arr, value) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    // console.log("times");
+    let mid_value = Math.floor((left + right) / 2);
+    if (value == arr[mid_value]) {
+      return mid_value;
+    } else if (arr[mid_value] < value) {
+      left = mid_value + 1;
+    } else {
+      right = mid_value - 1;
+    }
+  }
+  return -1;
+}
+
+console.log(
+  binarySearch(
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+    19
+  )
+);
+
+// time complexity O(log2(n))
+// space complexity O(1)
